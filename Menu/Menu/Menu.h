997b806232fc8538b10605a7d4a61bbadeb66d61
Andrei Yarmukhametov <andyarus@gmail.com>
2019-08-27 20:51:54 +0500

    app backbone added

---
+++ Menu/Menu/Menu.h
@@ -0,0 +1,19 @@
+//
+//  Menu.h
+//  Menu
+//
+//  Created by Andrei Coder on 27/08/2019.
+//  Copyright © 2019 Andrei Coder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Menu.
+FOUNDATION_EXPORT double MenuVersionNumber;
+
+//! Project version string for Menu.
+FOUNDATION_EXPORT const unsigned char MenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Menu/PublicHeader.h>
+
+
